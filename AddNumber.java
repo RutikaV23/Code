7e80e2355d5508caca475a9f813c5a2fb5c11e52
Rutika Vaidya <147318835+RutikaV23@users.noreply.github.com>
2025-05-11 16:52:37 +0530

    Update AddNumber.java

--- AddNumber.java
+++ AddNumber.java
@@ -4,15 +4,9 @@ public class AddNumber{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int n = sc.nextInt();
-        System.out.println("Enter the size of number:  ");
-        int size = sc.nextInt();
-
-        int[] arr = new int[size];
-        for(int i=0;i<size;i++){
-           arr[i] = n%10;
-           n = n/10;
+        int m = sc.nextInt();
         }
-        int sum = arr[0] + arr[size-1];
+        int sum = n + m;
         System.out.println("sum is: "+ sum);
     }
 }
