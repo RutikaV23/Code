7bd7e87b510c26081fd29c33e9cc711869cf606b
Rutika Vaidya <147318835+RutikaV23@users.noreply.github.com>
2025-05-11 17:28:14 +0530

    Update Discount.java

--- Discount.java
+++ Discount.java
@@ -2,14 +2,17 @@ import java.util.*;
 public class Discount{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("YOU ARE THE STUDENT : 1:YES  2:NO ");
+        System.out.println("YOU ARE THE Price ");
         int n = sc.nextInt();
         System.ou.println("YOU ARE THE STUDENT : 1:YES  2:NO ");
+        int n = sc.nextInt();
         switch(n){
-            case 1:
+            case 1:System.out.println("YES");
+                   StudentDiscount();
             break;
 
-            case 2:
+            case 2:System.out.println("NO ");
+                  Discount();
             break;
         }
     }
